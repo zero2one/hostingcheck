4a69f063b9f74400432b6c6410da891514a2d0d4
Peter Decuyper <github@serial-graphics.be>
2015-02-21 16:17:44 +0100

    Create an implementation of the collection interface with named collection items.

--- hostingcheck/Hostingcheck/Lib/Scenario.php
+++ hostingcheck/Hostingcheck/Lib/Scenario.php
@@ -15,75 +15,16 @@
  *
  * @author Peter Decuyper <peter@serial-graphics.be>
  */
-class Hostingcheck_Scenario extends Hostingcheck_Collection_Abstract
+class Hostingcheck_Scenario extends Hostingcheck_Collection_Keyed
 {
-    /**
-     * The group array keys.
-     *
-     * @var array
-     */
-    protected $keys = array();
-
-    /**
-     * Class constructor.
-     */
-    public function __construct()
-    {
-        $this->keys = array();
-        parent::__construct();
-    }
-
     /**
      * Add a group to the scenario.
      *
      * @param Hostingcheck_Scenario_Group $group
      */
-    public function add($group)
+    public function add(Hostingcheck_Scenario_Group $group)
     {
         $this->collection[$group->name()] = $group;
-        $this->keys = array_keys($this->collection);
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @return Hostingcheck_Scenario_Group
-     */
-    public function current() {
-        return $this->seek($this->key());
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @return string
-     */
-    public function key() {
-        return $this->keys[$this->position];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function valid() {
-        return isset($this->keys[$this->position]);
-    }
-
-    /**
-     * Get the group scenario by its name.
-     *
-     * @param string $name
-     *     The group name
-     *
-     * @return Hostingcheck_Scenario_Group|null
-     *     Returns null if the group does not exists.
-     */
-    public function seek($name)
-    {
-        if (!isset($this->collection[$name])) {
-            return null;
-        }
-
-        return $this->collection[$name];
+        $this->updateKeys();
     }
 }
