882110c4ccadc15676f63d1e16d5d7783f3f9804
Peter Decuyper <github@serial-graphics.be>
2015-03-12 22:58:11 +0100

    Removed license info from tests.

--- tests/Hostingcheck/Lib/Results/TestsTest.php
+++ tests/Hostingcheck/Lib/Results/TestsTest.php
@@ -1,14 +1,5 @@
 <?php
 /**
- * Hostingcheck (https://github.com/zero2one/hostingcheck)
- *
- * @link      https://github.com/zero2one/hostingcheck source repository.
- * @copyright Copyright (c) 2005-2014 Serial Graphics. (http://serial-graphics.be)
- * @license   http://opensource.org/licenses/GPL-2.0 GNU Public License
- */
-
-
-/**
  * Tests for Hostingcheck_Results_Tests class.
  *
  * @author Peter Decuyper <peter@serial-graphics.be>
